23b9121c571c89fa694e54d583b378c777679c4e
Gabriel Halus <gabrielhalus@gmail.com>
2026-01-09 16:30:10 +0100

    feat(shared): set value as string in RuntimeConfig type

--- packages/shared/src/types/db/runtime-configs.types.ts
+++ packages/shared/src/types/db/runtime-configs.types.ts
@@ -1,9 +1,8 @@
 import type { RuntimeConfigModel } from "~shared/db/models/runtime-configs.model";
-import type { Merge } from "~shared/lib/type-utils";
 
 export type ConfigValue = | string | number | boolean | null;
 
-export type RuntimeConfig = Merge<typeof RuntimeConfigModel.$inferSelect, { value: ConfigValue }>;
+export type RuntimeConfig = typeof RuntimeConfigModel.$inferSelect;
 
 export type ConfigNode = {
   key: string;
