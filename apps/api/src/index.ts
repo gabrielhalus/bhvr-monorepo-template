a526a96f5bed202fef720d7879722a7c0adb7303
Gabriel Halus <gabrielhalus@gmail.com>
2026-01-15 11:41:08 +0100

    feat(api): execute seeding strategy at start

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,4 +1,9 @@
 import app from "@/app";
+import { runSeeds } from "~shared/seeds";
+import { seeds } from "~seeds";
+
+// Run seeds before starting the server
+await runSeeds(seeds);
 
 // eslint-disable-next-line no-console
 console.log("🚀 Server is running on port 3000");
