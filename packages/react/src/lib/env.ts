ebe90e3849d4001a58c1ff4349485d1db4f332ed
Gabriel Halus <gabrielhalus@gmail.com>
2026-03-24 12:19:05 +0100

    refactor(react): remove env wrapper, include env.d.ts

--- packages/react/src/lib/env.ts
+++
@@ -1,9 +0,0 @@
-import { z } from "zod";
-
-import { validateEnv } from "~shared/env";
-
-export const env = validateEnv({
-  VITE_API_URL: z.string(),
-  VITE_SITE_URL: z.string(),
-  VITE_AUTH_URL: z.string(),
-});
